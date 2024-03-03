13441b0a9edc9cd3e58d6355ebd4ba03045ba0fa
anthonytk31415 <anthonytk31415@gmail.com>
2024-03-03 10:19:56 -0800

    finished linkedlist prob

--- python-fundamentals/linked-lists/removeNthFromEnd-v2.py
+++ python-fundamentals/linked-lists/removeNthFromEnd-v2.py
@@ -1,2 +1,30 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 def removeNthFromEnd(head, n):
+    dummy = ListNode()
+    dummy.next = head
+    node = dummy
+    countNodes = 0
+    while node: 
+        node = node.next
+        countNodes += 1
     
+    node = dummy
+    for _ in range(countNodes - n -1):
+        node = node.next
+    node.next = node.next.next
+    return dummy.next
+
+x = ListNode(1)
+# x.next = ListNode(2)
+# x.next.next = ListNode(3)
+# x.next.next.next = ListNode(4)
+# x.next.next.next.next = ListNode(5)
+
+y = removeNthFromEnd(x, 2)
+while y: 
+    print(y.val)
+    y = y.next
